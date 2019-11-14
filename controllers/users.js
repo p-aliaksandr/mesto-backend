45a9d4711b733cbf105f3a60bb59aabbb92e94e4
Sasha Prokofyev <ablehope@yandex.by>
2019-11-14 15:02:30 +0300

    add validation update func

--- controllers/users.js
+++ controllers/users.js
@@ -22,14 +22,14 @@ module.exports.createUser = (req, res) => {
 
 module.exports.updateProfile = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(404).send({ message: err.message }));
 };
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(404).send({ message: err.message }));
 };
