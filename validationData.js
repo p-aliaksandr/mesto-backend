428545fc32cf921e3ecc2966c60aae10c765b08c
Sasha Prokofyev <ablehope@yandex.by>
2019-12-18 13:14:37 +0300

    fixed file validationData

--- validationData.js
+++ validationData.js
@@ -5,14 +5,14 @@ const signupValidation = celebrate({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
     avatar: Joi.string().required(),
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 });
 
 const signinValidation = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 });
@@ -21,7 +21,6 @@ const cardsValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     link: Joi.string().required(),
-    owner: Joi.string().required(),
   }),
 });
 
